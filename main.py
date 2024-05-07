6762bfe4b4ea727dcd2376223c00ab2fbaa42941
Alexander Yoshida <alexander.yoshida@gmail.com>
2024-05-06 20:48:52 -0400

    disable mcts from main.py

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ def heuristic_placement(heuristic: Callable[[Placement],float], weighted_choice:
         render(state, score, line_clears)
         new_next_piece = Piece(PieceType(random.randint(0,6)))
         placements: list[Placement] = find_placements(state, new_next_piece, show_moves)
-        chosen = mcts_choose_placement(placements, heuristic, 5, 10, 10)
+        chosen = chose_placement(placements, heuristic, False, False)
         if show_moves: move_playback(state, chosen.moves, score, line_clears)
         score += chosen.score_gain
         line_clears += chosen.line_clears
